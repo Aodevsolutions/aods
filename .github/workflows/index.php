3b500d8422ef5ab1bc4feba63bcf37f4bcc83740
mabuselaaya <27950608+mabuselaaya@users.noreply.github.com>
2021-06-01 15:43:53 +0200

    Update index.php

--- .github/workflows/index.php
+++ .github/workflows/index.php
@@ -1,2 +1,59 @@
 <?php
 ?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>HlamvanaSkillsAcademy</title>
+<link href="tools/style.css" rel="stylesheet" type="text/css" media="screen" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<script type="text/javascript" src="tools/jquery.min.js"></script> 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.0.4/jquery.backstretch.min.js"></script> 
+<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:700|Roboto+Slab:700' rel='stylesheet' type='text/css'>
+<meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = no">
+
+
+</head>
+<body>
+<div class="wrapper">
+	<div class="left-section"><img src="images/watch.png" /></div>
+	<div class="right-section">
+		<h1 id="logo">HlamvanaSkillsAcademy</h1>
+		<div class="seprator"></div>
+		<div class="main-content">
+			<h2>Our Web Application is coming soon</h2>
+			<p></p>
+		</div>
+		<ul class="social">
+			<li id="facebook"><a href="https://www.facebook.com/HlamvanaSkillsAcademy"><i class="fa fa-facebook"></i></a></li>
+			<li id="twitter"><a href="https://twitter.com/HlamvanaSkillsAcademy"><i class="fa fa-twitter"></i></a></li>
+			<li id="instagram"><a href="https://www.instagram.com/HlamvanaSkillsAcademy/"><i class="fa fa-Instagram"></i></a></li>
+		</ul>
+		<ul class="info">
+			<li><i class="fa fa-phone-square"></i>(079) 840 7847</li>
+			<li><i class="fa fa-map-marker"></i>Gauteng, South Africa</li>
+			<li><i class="fa fa-envelope"></i><a href="mailto:HlamvanaSkillsAcademy@gmail.com"> HlamvanaSkillsAcademy@gmail.com</a></li>
+		</ul>
+
+		<div class="social-links">y
+			<ul class="social2">
+				<li id="facebook"><a href="https://www.facebook.com/HlamvanaSkillsAcademy/"><i class="fa fa-facebook"></i></a></li>
+				<li id="twitter"><a href="https://twitter.com/HlamvanaSkillsAcademy"><i class="fa fa-twitter"></i></a></li>
+				<li id="instagram"><a href="https://www.instagram.com/HlamvanaSkillsAcademy/"><i class="fa fa-instagram"></i></a></li>
+			</ul>
+		</div>
+		          <p class="text-black text-center tm-copyright-text">
+            Copyright &copy; 2021 Aodevsolutions App. 
+            Designed by <a href="https://aodevsolutions.com/" class="tm-copyright-link">AODS</a>
+          </p>
+		
+	</div>
+	<div class="watch-section"><img src="images/watch.png" /></div>
+</div>
+
+<!-- Page footer -->
+        
+
+
+
+</body>
+</html>
